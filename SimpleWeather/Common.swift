f6e5afa8b8ed555665b7ead5ae24c3774994a1e4
dopeheadz <djdope@naver.com>
2019-08-19 16:06:57 +0900

    SK 날씨 api 추가

---
+++ SimpleWeather/Common.swift
@@ -0,0 +1,12 @@
+//
+//  Common.swift
+//  SimpleWeather
+//
+//  Created by dope on 19/08/2019.
+//  Copyright © 2019 dope. All rights reserved.
+//
+
+import Foundation
+
+let apiKey = "c179df27-00da-4c16-83cd-65b71400de5c"
+
